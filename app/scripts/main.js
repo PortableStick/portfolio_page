23c26b23fe982f4a1a62791128b4a67d432f8755
Greg Smith <crashingwaves@fea.st>
2015-11-27 08:08:50 -0700

    Reorganized stylesheet after importing from old project; changed colors, and fixed spacing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+    "use strict";
+
+    var topOffset = 50;
+
+    $('body').scrollspy({
+        target: '#main-nav',
+        offset: topOffset
+    }).on('activate.bs.scrollspy',function(){
+        var loc = $('#main-nav').find('li.active a').attr('href');
+        if(loc !== '#landing'){
+            $('#main-nav').addClass('inbody');
+        } else {
+            $('#main-nav').removeClass('inbody');
+        }
+    });
+
+    !function(){    
+        var loc = $('#main-nav').find('li.active a').attr('href');
+        if(loc !== '#landing'){
+            $('#main-nav').addClass('inbody');
+        } else {
+            $('#main-nav').removeClass('inbody');
+        }
+    }()
+
+    $('.carousel').carousel({
+        pause: "false"
+    });
+})
+
+
