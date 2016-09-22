17a63abfcb43352a71a0afdcb188aa05d43d1c9d
Gregory Smith <crashingwaves@fea.st>
2016-09-22 12:12:48 -0600

    Add form submit action

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+require('bootstrap');
+$.noConflict(true);
+import toastr from 'toastr';
+import masonry from 'masonry-layout';
+import bridget from 'jquery-bridget';
+bridget('masonry', masonry, $);
+
 $(document).ready(function() {
     'use strict';
 
@@ -23,9 +32,13 @@ $(document).ready(function() {
         }
     }());
 
-    $('.carousel').carousel({
-        pause: 'false'
+    $("#email-form").on('submit', event => {
+        event.preventDefault();
+        $.post('http://localhost:9000/sendmail',$(event.target).serialize())
+            .done(result => {
+                console.log("Sent")
+            }).catch(error => {
+                console.error(error);
+            });
     });
 });
-
-
